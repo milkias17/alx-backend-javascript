bad523e1d22e07cd8bfe1ec626afc7340e694baf
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-01-01 09:27:02 +0300

    fix bugs in code

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -4,5 +4,5 @@ export default function appendToEachArrayValue(array, appendString) {
     arr.push(appendString + value);
   }
 
-  return array;
+  return arr;
 }
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -4,7 +4,7 @@ export default function createReportObject(employeesList) {
       ...employeesList,
     },
     getNumberOfDepartments() {
-      return Object.length(employeesList);
+      return Object.keys(employeesList).length;
     },
   };
 }
