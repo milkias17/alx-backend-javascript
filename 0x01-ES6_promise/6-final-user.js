974285bfd6d14573622b6c3d082c7d045ce41577
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-01-01 22:38:34 +0300

    fix bugs

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const resolveFunc = signUpUser(firstName, lastName);
-  const rejectFunc = uploadPhoto(fileName);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const resolveFunc = await signUpUser(firstName, lastName);
+  const rejectFunc = await uploadPhoto(fileName);
   return [resolveFunc, rejectFunc];
 }
