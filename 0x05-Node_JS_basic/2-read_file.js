9913d180dd930864baba93367b5ea7aff755e716
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-02-23 08:54:40 +0300

    do read file task

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,24 +5,24 @@ function countStudents(path) {
     throw new Error('Cannot load the database');
   }
 
-  const content = readFileSync(path, "utf8").trim().split("\n");
-  console.log(`Number of students: ${content.length - 1}`)
+  const content = readFileSync(path, 'utf8').trim().split('\n');
+  console.log(`Number of students: ${content.length - 1}`);
   const countHolder = new Map();
-  for (row of content.splice(1)) {
-    const tmp = row.split(",");
+  for (const row of content.splice(1)) {
+    const tmp = row.split(',');
     const fieldName = tmp[tmp.length - 1];
-    const studentName = tmp.splice(2).join(" ");
-    
+    const studentName = tmp.splice(0, 1).join(' ');
+
     if (countHolder.has(fieldName)) {
       countHolder.get(fieldName).push(studentName);
     } else {
-      countHolder.set(fieldName, [studentName])
+      countHolder.set(fieldName, [studentName]);
     }
   }
 
-  for (let [fieldName, students] of countHolder) {
-    
+  for (const [fieldName, students] of countHolder) {
+    console.log(`Number of students in ${fieldName}: ${students.length}. List: ${students.join(', ')}`);
   }
 }
 
-countStudents("database.csv")
+module.exports = countStudents;
